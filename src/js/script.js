ed4363b44f56b97ed29c2783a6c03dab1f3237a3
djjean <djjean90@gmail.com>
2021-06-15 08:54:16 +0300

    formValid

--- src/js/script.js
+++ src/js/script.js
@@ -30,12 +30,56 @@ $(document).ready(function(){
           $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
 
 
-        })
-      })
-    }
+        });
+      });
+    };
 
     toggleSlide(".catalog-item__link");
     toggleSlide(".catalog-item__back");
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay , #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut();
+
+    });
+    $('.button_mini').on('click',function(){
+      $('.overlay , #order').fadeIn();
+    });
+    $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay , #order').fadeIn();
+      });
+
+    });
+
+    function validateForms(form){
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required:true,
+            email:true
+          }
+        },
+        messages: {
+          name: "Пожалуйста, введите свое имя",
+          phone: "Введите номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+      });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
 
 });
